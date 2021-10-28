c09a8a186e7963280c23c93674b6d73a15878e59
Abhin Krishna K A <abhinkrishna.ka@gmail.com>
2021-10-29 01:07:18 +0530

    add packages

--- lib/sqflite_live.dart
+++ lib/sqflite_live.dart
@@ -1,7 +1,27 @@
 library sqflite_live;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart' as p;
+
+class SqfliteLive {
+  late final Database _database;
+
+  Future<Database> open(String dbName, String createSql, {int version = 1, bool readOnly = false, bool newInstance = false}) async {
+    _database = await openDatabase(
+      p.join(await getDatabasesPath(), "$dbName.db"),
+      version: version,
+      readOnly: readOnly,
+      singleInstance: !newInstance,
+      onCreate: (Database db, int version) => db.execute(createSql),
+    );
+    return _database;
+  }
+
+  void close() async {
+    await _database.close();
+  }
+
+  void delete(String dbName) async {
+    await deleteDatabase(p.join(await getDatabasesPath(), "$dbName.db"));
+  }
 }
