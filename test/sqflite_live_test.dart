64f05844c6612b2945d5cfaba4556e86a243c83e
Abhin Krishna K A <abhinkrishna.ka@gmail.com>
2021-10-30 18:57:26 +0530

    update test file

--- test/sqflite_live_test.dart
+++ test/sqflite_live_test.dart
@@ -1,12 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_live/sqflite_live.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('try opening database ', () async {
+    SqfliteLive instance = SqfliteLive();
+    var db = await instance.open('test_database', ''' CREATE TABLE ( test id INTEGER PRIMARY KEY AUTOINCREMENT ) ''');
+    expect(db.runtimeType, Database);
+    expect(db.path.runtimeType, String);
+    expect(db.insert("test_database", {}), 1);
   });
 }
