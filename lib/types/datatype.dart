2085d7f7147d407a00abac85af2d972be5c2b634
Abhin Krishna K A <abhinkrishna.ka@gmail.com>
2021-11-08 01:32:30 +0530

    added types definitions

--- lib/types/datatype.dart
+++ lib/types/datatype.dart
@@ -1,8 +1,19 @@
 enum ColumnType {
+  // Support for integer type
   integer,
+  // Support for textual data
   text,
+  // Support for boolean [true, false]. SQLite doesn't have
+  // support for boolean type but booleans values are converted
+  // into integers true -> 1 ; false -> 0
   boolean,
+  // Support for json type. SQLite doesn't have support for json
+  // Json is stored as string after stringifying a json with jsonEncode()
   json,
+  // Support for list type. SQLite doesn't have support for list
+  // List is stored as string after stringifying with jsonEncode()
   list,
+  // Support for double type. SQLite doesn't have support for double
+  // double is stored as string and parsed back to double 
   double,
 }
--- lib/types/operation.dart
+++ lib/types/operation.dart
@@ -1,7 +1,11 @@
 
 enum OperationType {
+  // Create operation had done.
   create,
+  // Update operation had done.
   update,
+  // Delete operation had done.
   delete,
+  // Select operation had done.
   select,
 }
