21855c5e7bbaa781795ebb4b1cbd0420043b80ec
Abhin Krishna K A <abhinkrishna.ka@gmail.com>
2021-11-12 07:58:48 +0530

    update sqflite test case

--- test/sqflite_live_test.dart
+++ test/sqflite_live_test.dart
@@ -4,8 +4,8 @@ import 'package:sqflite_live/sqflite_live.dart';
 
 void main() {
   test('try opening database ', () async {
-    SqfliteLive instance = SqfliteLive.instance;
-    var db = await instance.open('test_database', ''' CREATE TABLE ( test id INTEGER PRIMARY KEY AUTOINCREMENT ) ''');
+    SqfliteLive sqfliteLive = SqfliteLive('test_database', createTableQueries: ['CREATE TABLE ( test id INTEGER PRIMARY KEY AUTOINCREMENT )']);
+    var db = sqfliteLive.db;
     expect(db.runtimeType, Database);
     expect(db.path.runtimeType, String);
     expect(db.insert("test_database", {}), 1);
